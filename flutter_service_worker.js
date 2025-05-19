b124d2d33dc252bf42c7e744586b25f53589dd12
SilvioCesar73 <154388417+SilvioCesar73@users.noreply.github.com>
2025-05-18 23:28:47 -0300

    Atualizacao

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,16 +32,15 @@ const RESOURCES = {"assets/AssetManifest.bin": "3a880347b142cc7d732075d18d053da3
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ae5e17d430388b53280e0b4a925915b7",
+"flutter_bootstrap.js": "54add8f4e0eb7a58177ed76aeb42b25f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "509c714adffd1e2b3a252ac2829ad688",
 "/": "509c714adffd1e2b3a252ac2829ad688",
-"main.dart.js": "3a41ae183f2d73cb0160ed20ca6a7647",
+"main.dart.js": "9aa76a9c671ab6f3a98dcb59155ea492",
 "manifest.json": "ec6f74390a8b07b9a8e56ca1eb82520c",
-"traumanurse_web.zip": "29d918f56a2c1be136bac1a8191602c9",
 "version.json": "271ee56bf6df30309f1ed3473e091823"};
 // The application shell files that are downloaded before a service worker can
 // start.
